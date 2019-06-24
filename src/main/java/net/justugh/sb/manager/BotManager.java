0f29d71fcdd069c77f7562d6a9fed84a775c00b1
Justin <justughv@gmail.com>
2019-06-24 19:49:33 -0400

    Add auto role assignment for bot Discord

--- src/main/java/net/justugh/sb/manager/BotManager.java
+++ src/main/java/net/justugh/sb/manager/BotManager.java
@@ -1,6 +1,7 @@
 package net.justugh.sb.manager;
 
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.justugh.sb.Bot;
 
@@ -16,5 +17,14 @@ public class BotManager extends ListenerAdapter {
 
         Bot.getInstance().getGuildConfigCache().get(event.getGuild().getIdLong()).setDefaultSuggestionChannel(event.getGuild().getDefaultChannel().getIdLong());
         Bot.getInstance().getGuildConfigCache().get(event.getGuild().getIdLong()).save();
+    }
+
+    @Override
+    public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event) {
+        if(event.getGuild().getIdLong() != 592031551777406977L) {
+            return;
+        }
+
+        event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById(592846541082066953L)).queue();
     }
 }
